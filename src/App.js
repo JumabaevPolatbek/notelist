98fa46f794ad55a53e3dab4270deda0d2b6a5a36
JumabaevPolatbek <mouz.cyx@mail.ru>
2022-09-10 20:09:55 +0500

    notelist

---
+++ src/App.js
@@ -0,0 +1,71 @@
+// import logo from './logo.svg';
+import "./App.css";
+import React from "react";
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    var arr = [];
+    this.arr = arr;
+    this.state = {
+      note: [],
+    };
+    this.addNote = this.addNote.bind(this);
+    this.close = this.close.bind(this);
+    this.TodoItem = this.TodoItem.bind(this);
+    this.checkedNote = this.checkedNote.bind(this);
+  }
+  addNote() {
+    const text = document.querySelector(".input");
+    if (text.value !== "") {
+      this.arr.push(text.value);
+    }
+    this.setState({
+      note: this.arr,
+    });
+    text.value = "";
+  }
+  checkedNote(e) {
+    var elem = e.target.parentElement;
+    e.target.checked === true
+      ? (elem.style.textDecoration = "line-through")
+      : (elem.style.textDecoration = "inherit");
+  }
+  close(e) {
+    e.preventDefault();
+    e.target.parentElement.remove();
+  }
+  TodoItem({ props }) {
+    const styleCursor = {
+      cursor: "pointer",
+      display: "flex",
+      justifyContent: "space-between",
+    };
+    return (
+      <div style={styleCursor}>
+        <input type="checkbox" onClick={this.checkedNote} />
+        <span>{props}</span>
+        <button onClick={this.close}>X</button>
+      </div>
+    );
+  }
+  render() {
+    const todo = {
+      width: "300px",
+      margin: "50px auto",
+    };
+    return (
+      <div style={todo}>
+        {this.state.note.map((item, index) => {
+          return <this.TodoItem props={item} key={index} />;
+        })}
+        <div>
+          <input type="text" className="input" />
+          <button onClick={this.addNote}>Add Note</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ToDo;
